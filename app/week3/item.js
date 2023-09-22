cd8a9a424ec46a45315547dbe4bbde496076877c
Kyle Castillo <kyle.castillo@edu.sait.ca>
2023-09-22 16:03:31 -0600

    Update item.js

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,8 @@
 export default function Items( {name, quantity, category }) {
+    //Outputs items from Item list
     return (
         <div className="border-spacing-0 m-5 bg-sky-400 w-[250px] p-3">
-            <h2>Item: {name}</h2> //Outputs name of item
+            <h2>Item: {name}</h2> 
             <p>Quantity: {quantity}</p>
             <p>Category: {category}</p>
         </div>
