55a6a0eeb07f19fead46a349e4b07665958274d5
Kyle Castillo <kyle.castillo@edu.sait.ca>
2023-10-13 15:25:23 -0600

    Update item-list.js

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -2,4 +2,54 @@
 
 import { useState } from "react";
 import Items from "./item";
-import it
+import itemsData from "./items.json"
+
+export default function ItemList() {
+    const [sortBy, setSortBy] = useState("name");
+
+    let items = itemsData.map((item) => ({
+        ...item}));
+    
+
+   const handleSortByChange = (newSortPreference) => {
+    setSortBy(newSortPreference);
+   }
+
+   return (
+    <div>
+        <div>
+            <h3>
+                Sort By:
+            </h3>
+            <button
+                onClick={() => handleSortByChange("name")}
+                style={{backgroundColor: sortBy === "name" ? "lightblue" : "white"}}
+            >
+                Name
+            </button> 
+            <button
+                onClick={() => handleSortByChange("category")}
+                style={{ backgroundColor: sortBy === "category" ? "lightgrey" : "white"}}
+            >
+              Category  
+            </button> 
+        </div>
+
+        <ul>
+            {Items
+            .sort((a,b) => {
+                if (sortBy === "name") {
+                    return a.name.localeCompare(b.name);
+                } else if (sortBy === "category") {
+                    return a.category.localeCompare(b.category);
+                }
+            })
+            .map(item => (
+                <Item key={item.id} item={item} />
+            ))
+            }
+        </ul>
+    </div>
+   );
+}
+
